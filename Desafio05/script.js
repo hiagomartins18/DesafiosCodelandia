208e10964a154fbd931c01793cf6fb714f8846e7
Hiago Martins <hiagomartinsslv@gmail.com>
2023-03-05 16:37:31 -0300

    Update 1

---
+++ Desafio05/script.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset: true, distance: '120px' });
+
+ScrollReveal().reveal('.left', {
+  duration: 3000,
+  origin: 'left',
+})
+ScrollReveal().reveal('.right', {
+  duration: 3000,
+  origin: 'right',
+})
